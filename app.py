9e00202a0d5a549a7d7cadb88a34266928416ee9
mandashivakumar <shivakumar.manda5@gmail.com>
2025-03-26 19:17:31 +0000

    changed to 3 columns

--- app.py
+++ app.py
@@ -228,23 +228,25 @@ def main_app():
     st.markdown('<div class="subheader">Your Details</div>', unsafe_allow_html=True)
 
     # Inputs in two columns
-    col1, col2 = st.columns(2)
+    col1, col2, col3 = st.columns(3)
     with col1:
         age = st.number_input("Age", min_value=0, max_value=100)
-        gender = st.selectbox("Gender", ["Male", "Female"])
-        weight = st.number_input("Weight (kg)", min_value=0.0)
         height = st.number_input("Height (m)", min_value=0.0)
-        max_bpm = st.number_input("Max BPM", min_value=0)
         avg_bpm = st.number_input("Avg BPM", min_value=0)
-        resting_bpm = st.number_input("Resting BPM", min_value=0)
+        fat_percentage = st.number_input("Fat Percentage", min_value=0.0)
+        bmi = st.number_input("BMI", min_value=0.0)
 
     with col2:
+        gender = st.selectbox("Gender", ["Male", "Female"])
+        max_bpm = st.number_input("Max BPM", min_value=0)
         session_duration = st.number_input("Session Duration (hours)", min_value=0.0)
-        calories_burned = st.number_input("Calories Burned", min_value=0)
-        fat_percentage = st.number_input("Fat Percentage", min_value=0.0)
         water_intake = st.number_input("Water Intake (liters)", min_value=0.0)
+
+    with col3:
+        weight = st.number_input("Weight (kg)", min_value=0.0)
+        resting_bpm = st.number_input("Resting BPM", min_value=0)
+        calories_burned = st.number_input("Calories Burned", min_value=0)
         workout_frequency = st.number_input("Workout Frequency", min_value=0)
-        bmi = st.number_input("BMI", min_value=0.0)
 
     st.markdown('</div>', unsafe_allow_html=True)  # Close the bordered container
 
